1d12af0ba1cf971c1f35cbc293b79f62b02acaea
Alex Dresco <a.dresco@elsevier.com>
2019-04-21 11:15:01 +0100

    Changed from model to service

--- src/main/java/com/drescoding/offers/service/ProductRepository.java
+++ src/main/java/com/drescoding/offers/service/ProductRepository.java
@@ -1,7 +1,8 @@
-package com.drescoding.offers.model;
+package com.drescoding.offers.service;
 
 import java.util.List;
 
+import com.drescoding.offers.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
--- src/test/java/com/drescoding/offers/unit/ProductRepositoryTest.java
+++ src/test/java/com/drescoding/offers/unit/ProductRepositoryTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 import com.drescoding.offers.model.Product;
-import com.drescoding.offers.model.ProductRepository;
+import com.drescoding.offers.service.ProductRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
