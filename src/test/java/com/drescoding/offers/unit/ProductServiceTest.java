4af3cb22dd7dd74a935045871fea0bcb0338c57a
Alex Dresco <a.dresco@elsevier.com>
2019-04-22 09:30:19 +0100

    Refactored and TDD for expiry logic

--- src/test/java/com/drescoding/offers/unit/ProductServiceTest.java
+++ src/test/java/com/drescoding/offers/unit/ProductServiceTest.java
@@ -12,25 +12,19 @@ import com.drescoding.offers.model.Product;
 import com.drescoding.offers.service.ProductRepository;
 import com.drescoding.offers.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
 import org.junit.Test;
 
 @Slf4j
 public class ProductServiceTest {
 
-  @Before
-  public void initialise() {
-
-  }
+  ProductRepository productRepository = mock(ProductRepository.class);
+  Product product1 = new Product(1, "Dress", "EUR", 10.0, "Nice dress", "10/11/2019", true);
+  List<Product> listOfProducts = new ArrayList<>();
 
   @Test
   public void getProductByNameTest() {
     //Given
-    Product product1 = new Product(1, "Dress", "EUR", 10.0, "Nice dress", "10/11/2019", true);
-    List<Product> listOfProducts = new ArrayList<Product>();
     listOfProducts.add(product1);
-
-    ProductRepository productRepository = mock(ProductRepository.class);
     when(productRepository.getProductByName("Dress")).thenReturn(listOfProducts);
     ProductService productService = new ProductService(productRepository);
 
@@ -44,8 +38,6 @@ public class ProductServiceTest {
   @Test
   public void getProductByIdTest() {
     //Given
-    Product product1 = new Product(1, "Dress", "EUR", 10.0, "Nice dress", "10/11/2019", true);
-    ProductRepository productRepository = mock(ProductRepository.class);
     when(productRepository.findById(1)).thenReturn(Optional.of(product1));
     ProductService productService = new ProductService(productRepository);
 
@@ -56,19 +48,31 @@ public class ProductServiceTest {
   }
 
   @Test
-  public void findAllTest(){
-    Product product1 = new Product(1, "Dress", "EUR", 10.0, "Nice dress", "10/11/2019", true);
-    List<Product> listOfProducts = new ArrayList<Product>();
-    listOfProducts.add(product1);
-
-    ProductRepository productRepository = mock(ProductRepository.class);
+  public void findAllTest() {
+    //Given
     when(productRepository.findAll()).thenReturn(listOfProducts);
     ProductService productService = new ProductService(productRepository);
 
+    //When
     List<Product> products = productService.getAllProducts();
 
     //Then
     assertEquals(listOfProducts, products);
+  }
+
+  @Test
+  public void shouldBeCancelledAfterTheExpiryDate() {
+    //Given
+    ProductService productService = new ProductService(productRepository);
+    Product expiredProduct = new Product(1, "Dress", "EUR", 10.0, "Nice dress", "10/11/2018", true);
+
+    //When
+    Product product = productService.getProductById(1);
+
+    //Then
+    assertEquals(false, product.getValid());
+
+
   }
 
 }
