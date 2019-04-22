cff85675d8024e6afdc1b536c04a0bf807ce474c
Alex Dresco <a.dresco@elsevier.com>
2019-04-22 10:35:52 +0100

    Reloading the data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into product
+values(1, 'Dress', 'EUR', '1/1/2018', 'TestDescription', 2.0, false);
+
+insert into product
+values(2, 'Jeans', 'EUR', '1/1/2020', 'TestDescription', 5.0, true);
+
+-- Loads the data when the application is running.
